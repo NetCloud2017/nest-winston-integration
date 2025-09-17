3e03eea1e6a7b90da43c8244c8c0f8b29d18c18e
NetCloud2017 <tothinktodo@163.com>
2025-09-17 17:26:34 +0800

    feat: 打印日志添加颜色

--- src/myLogger.ts
+++ src/myLogger.ts
@@ -1,32 +1,57 @@
-import { ConsoleLogger, LoggerService, LogLevel } from '@nestjs/common';
-import { createLogger, format, Logger, transports } from 'winston';
+import {ConsoleLogger, LogLevel , LoggerService} from '@nestjs/common';
+import { createLogger , format, Logger, transports } from 'winston'; 
+import * as chalk from 'chalk'
+import * as dayjs from 'dayjs'
 
 export class MyLogger implements LoggerService {
+  private logger: Logger;
+  constructor () {
 
-    private logger: Logger;
-
-    constructor() {    
-        this.logger = createLogger({
-            level: 'debug',
+    this.logger = createLogger({
+      level: 'debug',
+      format: format.combine(
+        format.colorize(),
+        format.simple()
+      ),
+      transports: [
+        new transports.Console(
+          {
             format: format.combine(
-                format.colorize(),
-                format.simple()
+              format.colorize(),
+              format.printf(({level, message, context, time}) => {
+                const appStr = chalk.green(`[NEST]`);
+                const contextStr = chalk.yellow(`[${context}]`)
+                return `${appStr} ${time} ${level} ${contextStr} ${message}`
+              })
+            )
+          }
+        ),
+        new transports.File({
+            format: format.combine(
+                format.timestamp(),
+                format.json()
             ),
-            transports: [
-                new transports.Console()
-            ]
-        });
-    }
+            filename: '111.log',
+            dirname: 'log'
+        })
+      ]
+    })
+  }
+  log(message: string, context: string) {
+      const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
 
-    log(message: string, context: string) {
-        this.logger.log('info', `[${context}] ${message}`);
-    }
+      this.logger.log('info', message, { context, time });
+  }
 
-    error(message: string, context: string) {
-        this.logger.log('error', `[${context}] ${message}`);
-    }
+  error(message: string, context: string) {
+      const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
 
-    warn(message: string, context: string) {
-        this.logger.log('warn', `[${context}] ${message}`);
-    }
+      this.logger.log('info', message, { context, time });
+  }
+
+  warn(message: string, context: string) {
+      const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+
+      this.logger.log('info', message, { context, time });
+  }
 }
